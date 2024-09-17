51e827774f3cd84a9e5438a64c3670c71f9a6a7e
Kaylee-Baichulall-ICD2O1-01 <kaylee.baichulall@student.tdsb.on.ca>
2024-09-17 13:54:08 +0000

    commit one

--- PP1_3.py
+++ PP1_3.py
@@ -1,14 +1,25 @@
-
+'''
+Lesson 3 - Using Strings
+Author: Kaylee Baichulall
+Date Created: September 17th, 2024
+Date Last Modified: September 17th, 2024
+'''
 
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
+  print(" * * * * * *\n* * * * * * \n * * * * * *\n* * * * * * \n * * * * * *\n* * * * * * \n")
+  print("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
+  print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
 
 def q2():
   #Write Assignment code here
+  print("Hello ", end = "")
+  print("World\n")
   
 def q3():
   #Write Assignment code here
+  print("\"Man\"\n\"Board\"\n\"Man Overboard\"")
   
 
 
